4933509814596f4bb6c80f3dde252e178ec8f751
JMAEX <jovenm1125@gmail.com>
2022-09-13 13:18:14 -0500

    ia

--- codigo/code6.js
+++ codigo/code6.js
@@ -1,48 +1,65 @@
 //scenario
 const scene = new THREE.Scene();
 scene.background = new THREE.Color(0x000000);
+/* scene.fog = new THREE.Fog(0xffffff, 5, 7); */
 
 //fondo
-var loader = new  THREE.TextureLoader();
-loader.load("../Img/fondo.jpg", function(texture){
-   scene.background = texture;
+var loader = new THREE.TextureLoader();
+loader.load("../Img/fondo.jpg", function (texture) {
+	scene.background = texture;
 });
 
 
 //camara
-const camera = new THREE.PerspectiveCamera( 100, window.innerWidth / window.innerHeight,);
+const camera = new THREE.PerspectiveCamera(100, window.innerWidth / window.innerHeight,);
 
 
 //render
 const renderer = new THREE.WebGLRenderer();
-renderer.setSize( window.innerWidth, window.innerHeight );
-document.body.appendChild( renderer.domElement );
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
 
 //geometria
 
 const TextureLoader = new THREE.TextureLoader();
 const matcap = TextureLoader.load("/Img/textura1.jpg");
 
-const geometry = new THREE.BoxGeometry( 3, 3, 3 );
+const verticesOfCube = [
+    -1,-1,-1,    1,-1,-1,    1, 1,-1,    -1, 1,-1,
+    -1,-1, 1,    1,-1, 1,    1, 1, 1,    -1, 1, 1,
+];
 
-const edges = new THREE.EdgesGeometry( geometry );
-const line = new THREE.LineSegments( edges, new THREE.LineBasicMaterial( { color: 0xffffff } ) );
-scene.add( line );
+const indicesOfFaces = [
+    2,1,0,    0,3,2,
+    0,4,7,    7,3,0,
+    0,1,5,    5,4,0,
+    1,2,6,    6,5,1,
+    2,3,7,    7,6,2,
+    4,5,6,    6,7,4
+];
 
-const material = new THREE.MeshMatcapMaterial();
-material.color.set('#fff')
-material.matcap = matcap;
+const geometry = new THREE.PolyhedronGeometry( verticesOfCube, indicesOfFaces, 6, 2 );
+
+const edges = new THREE.EdgesGeometry(geometry);
+const line = new THREE.LineSegments(edges, new THREE.LineBasicMaterial({ color: 0xffffff }));
+scene.add(line);
+
+const material = new THREE.MeshStandardMaterial();
+material.metalness = 1;
+material.roughness = 0.2;
 material.flashading = true;
-const cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
+const cube = new THREE.Mesh(geometry, material);
+scene.add(cube);
 
-camera.position.z =7;
+camera.position.z = 10;
 
 //animación
 function animate() {
 	requestAnimationFrame( animate );
 	line.rotation. y += 0.01;
+	line.rotation. x += 0.01;
 	cube.rotation.y += 0.01;
+	cube.rotation.x += 0.01;
 	renderer.render( scene, camera );
 }
 animate();
--- codigo/code7.js
+++ codigo/code7.js
@@ -1,48 +1,49 @@
 //scenario
 const scene = new THREE.Scene();
 scene.background = new THREE.Color(0x000000);
+scene.fog = new THREE.Fog(0xffffff, 5, 7);
 
 //fondo
-var loader = new  THREE.TextureLoader();
-loader.load("../Img/fondo.jpg", function(texture){
-   scene.background = texture;
+var loader = new THREE.TextureLoader();
+loader.load("../Img/fondo.jpg", function (texture) {
+	scene.background = texture;
 });
 
 
 //camara
-const camera = new THREE.PerspectiveCamera( 100, window.innerWidth / window.innerHeight,);
+const camera = new THREE.PerspectiveCamera(100, window.innerWidth / window.innerHeight,);
 
 
 //render
 const renderer = new THREE.WebGLRenderer();
-renderer.setSize( window.innerWidth, window.innerHeight );
-document.body.appendChild( renderer.domElement );
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
 
 //geometria
 
 const TextureLoader = new THREE.TextureLoader();
 const matcap = TextureLoader.load("/Img/textura1.jpg");
 
-const geometry = new THREE.BoxGeometry( 3, 3, 3 );
+const geometry = new THREE.TorusGeometry( 10, 3, 16, 100 );
 
-const edges = new THREE.EdgesGeometry( geometry );
-const line = new THREE.LineSegments( edges, new THREE.LineBasicMaterial( { color: 0xffffff } ) );
-scene.add( line );
+const edges = new THREE.EdgesGeometry(geometry);
+const line = new THREE.LineSegments(edges, new THREE.LineBasicMaterial({ color: 0xffffff }));
+scene.add(line);
 
-const material = new THREE.MeshMatcapMaterial();
-material.color.set('#fff')
-material.matcap = matcap;
+const material = new THREE.MeshNormalMaterial();
 material.flashading = true;
-const cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
+const cube = new THREE.Mesh(geometry, material);
+scene.add(cube);
 
-camera.position.z =7;
+camera.position.z = 20;
 
 //animación
 function animate() {
 	requestAnimationFrame( animate );
 	line.rotation. y += 0.01;
+	line.rotation. x += 0.01;
 	cube.rotation.y += 0.01;
+	cube.rotation.x += 0.01;
 	renderer.render( scene, camera );
 }
 animate();
